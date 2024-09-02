9da03e9a610f27ca06b3fd60bd0a952f2e290915
Justin Tzu-Hung Lin <hozion612356@gmail.com>
2024-09-01 21:52:47 -0700

    Fix environment reading problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL,
+    MY_VAR: process.env.MY_VAR,
+  },
+};
 
 export default nextConfig;
